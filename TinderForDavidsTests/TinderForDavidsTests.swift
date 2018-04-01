0cda39d3c4dc48b2289ef953ea4b09065d383f81
dakin4 <davekng0001@gmail.com>
2018-03-31 21:06:44 -0400

    Initial Commit

---
+++ TinderForDavidsTests/TinderForDavidsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TinderForDavidsTests.swift
+//  TinderForDavidsTests
+//
+//  Created by David King on 3/31/18.
+//  Copyright © 2018 David King. All rights reserved.
+//
+
+import XCTest
+@testable import TinderForDavids
+
+class TinderForDavidsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
